04e9f954f53094c0975ef5b5e249e4496cf1b48e
PilotDevo <devo5676@gmail.com>
2022-08-29 19:55:48 -0400

    Update base.py

--- config/settings/base.py
+++ config/settings/base.py
@@ -5,12 +5,12 @@ from pathlib import Path
 
 import environ
 
-ROOT_DIR = Path(__file__).resolve(strict=True).parent.parent.parent
+ROOT_DIR = Path(__file__).resolve(strict=False).parent.parent.parent
 # tdmads/
 APPS_DIR = ROOT_DIR / "tdmads"
 env = environ.Env()
 
-READ_DOT_ENV_FILE = env.bool("DJANGO_READ_DOT_ENV_FILE", default=True)
+READ_DOT_ENV_FILE = env.bool("DJANGO_READ_DOT_ENV_FILE", default=False)
 if READ_DOT_ENV_FILE:
     # OS environment variables take precedence over variables from .env
     env.read_env(str(ROOT_DIR / ".env"))
